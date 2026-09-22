8dadd02a6dba948132f81091510b89e8a380db18
itmaster1000000-dotcom <itmaster.1000000@gmail.com>
2026-09-22 17:06:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const SUPABASE_URL =
   'https://ypnpeiglbiycbexpeibb.supabase.co';
 
 const SUPABASE_PUBLISHABLE_KEY =
-  'PASTE_YOUR_SUPABASE_PROJECT_PUBLISHABLE_OR_ANON_KEY_HERE';
+  'sb_publishable_5ei0IhM1tQ15u5pAIIavhQ_alE9O3bl';
 
 const WHATSAPP_NOTIFY_ENDPOINT = '';
 
