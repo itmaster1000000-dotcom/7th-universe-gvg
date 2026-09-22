83ee999fe8922eaa84dade6d6c06134e3ecd8775
itmaster1000000-dotcom <itmaster.1000000@gmail.com>
2026-09-22 17:19:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@
   NEVER put a service_role/secret key here.
 */
 
-const SUPABASE_URL = 'PASTE_YOUR_SUPABASE_PROJECT_URL_HERE';
-const SUPABASE_PUBLISHABLE_KEY = 'PASTE_YOUR_SUPABASE_PUBLISHABLE_OR_ANON_KEY_HERE';
+const SUPABASE_URL = 'https://ypnpeiglbiycbexpeibb.supabase.co';
+const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_5ei0IhM1tQ15u5pAIIavhQ_alE9O3bl';
 const WHATSAPP_NOTIFY_ENDPOINT = '';
 
 const { createClient } = window.supabase;
